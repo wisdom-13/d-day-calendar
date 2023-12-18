84255ccbeab7a6d79f358bb03830bf1f905dd63e
jhlee <wisdom_13_@naver.com>
2023-12-18 17:29:13 +0900

    sanity 설치

---
+++ sanity-studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'b6rkaw8u',
+    dataset: 'production'
+  }
+})
---
+++ sanity-studio/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'd-day-calendar',
+
+  projectId: 'b6rkaw8u',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
