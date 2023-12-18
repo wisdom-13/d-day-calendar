b922eb3309523ce596e6dd62ff5c73b5011c7fee
jhlee <wisdom_13_@naver.com>
2023-12-18 17:29:29 +0900

    user 스키마 생성

---
+++ sanity-studio/schemas/user.ts
@@ -0,0 +1,29 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string'
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string'
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string'
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'email',
+      // media: 'image'
+    }
+  }
+}
